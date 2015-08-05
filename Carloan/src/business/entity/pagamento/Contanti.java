152181b60e3e67f247591121d6f2fff4a2dc966e
francesco <frankgiak@gmail.com>
2015-08-05 18:08:45 +0200

    Aggiunto Carta di credito , contanti e  in business.entity sesso

---
+++ Carloan/src/business/entity/pagamento/Contanti.java
@@ -0,0 +1,10 @@
+package business.entity.pagamento;
+
+public class Contanti extends Pagamento{
+
+	public Contanti(float depositoCauzinale, float importo,
+			float detrazioneAggiuntiva) {
+		super(depositoCauzinale, importo, detrazioneAggiuntiva);
+	}
+
+}
