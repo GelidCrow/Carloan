f75842b6f1f549860d055f26676898ae25e0ec31
francesco <frankgiak@gmail.com>
2015-08-06 12:46:41 +0200

    Systeming

--- Carloan/src/integration/DAO/MySqlDaoFactory.java
+++ Carloan/src/integration/DAO/MySqlDaoFactory.java
@@ -1,6 +1,8 @@
 package integration.DAO;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import config.ConfiguratorDBReader;
 import config.ConfiguratorReader;
@@ -23,6 +25,17 @@ public class MySqlDaoFactory extends DaoFactory{
 	private static Connection con;
 
 	private ConfiguratorReader reader;
+	/**
+	 * Fornisce l'unica istanza di MySqlDaoFactory.
+	 * 
+	 * @return l'istanza di MySqlDaoFactory.
+	 */
+	public static MySqlDaoFactory getIstance() {
+		if (dao == null) {
+			dao = new MySqlDaoFactory();
+		}
+		return dao;
+	}	
 	
 	/**
 	 * Costruttore privato per la realizzazione del Singletone.
@@ -35,17 +48,31 @@ public class MySqlDaoFactory extends DaoFactory{
 		password = reader.getField(ConfiguratorDBReader.PASSWORD);
 		connessione = "jdbc:mysql://" + host + "/" + db;
 	}
+	/**
+	 * Permette di aprire una connessione al database.
+	 * 
+	 * @return la connessione creata.
+	 * @throws SQLException
+	 */
+	public static Connection connetti() throws SQLException {
+		getIstance();
+		con = DriverManager.getConnection(connessione, username, password);
+		return con;
+	}
+
+	/**
+	 * Chiude la connessione al database.
+	 * @throws SQLException 
+	 * 
+	 */
+	public static void chiudiConnessione() throws SQLException {
+		if (con != null) {
+			if (!con.isClosed()) {
+				con.close();
+			}
+		}
+	}
+	
 
 	
-	/**
-	 * Fornisce l'unica istanza di MySqlDaoFactory.
-	 * 
-	 * @return l'istanza di MySqlDaoFactory.
-	 */
-	public static MySqlDaoFactory getIstance() {
-		if (dao == null) {
-			dao = new MySqlDaoFactory();
-		}
-		return dao;
-	}	
 }
