31eb5e9ec884f4c8bbad5e1785cdf2d538f373ca
francesco <frankgiak@gmail.com>
2015-08-11 17:09:07 +0200

    Main aggiornato

--- Carloan/src/presentation/mvp/boundary/view/Main.java
+++ Carloan/src/presentation/mvp/boundary/view/Main.java
@@ -1,13 +1,28 @@
 package presentation.mvp.boundary.view;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+		try {
+			Parent root = (Parent)FXMLLoader.load(Main.class.getResource("/markup/Login.fxml"));
+			Scene scene= new Scene(root,350,200);
+			primaryStage.setScene(scene);
+			primaryStage.show();	
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 	}
 
 	public static void main(String[] args) {
