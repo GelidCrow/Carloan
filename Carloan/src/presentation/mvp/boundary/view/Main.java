9d7e32371c591ca648bb051136a57985e43bf44b
Gelidcrow <gelidcrow@gmail.com>
2015-08-11 17:19:52 +0200

    asd

--- Carloan/src/presentation/mvp/boundary/view/Main.java
+++ Carloan/src/presentation/mvp/boundary/view/Main.java
@@ -4,27 +4,30 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			Parent root = (Parent)FXMLLoader.load(Main.class.getResource("/markup/Login.fxml"));
-			Scene scene= new Scene(root,350,200);
-			primaryStage.setScene(scene);
-			primaryStage.show();	
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+		this.primaryStage=primaryStage;
+		this.primaryStage.setTitle("CarLoan");
+		initRootLayout();
 	}
-
+public void initRootLayout(){
+	try{
+		FXMLLoader loader=new FXMLLoader();
+		loader.setLocation(Main.class.getResource("Login.fxml"));
+		Scene scene=new Scene(loader.load());
+		this.primaryStage.setScene(scene);
+		this.primaryStage.show();
+	}
+	catch(IOException e){
+		e.printStackTrace();
+	}
+}
 	public static void main(String[] args) {
 		launch(args);
 	}
