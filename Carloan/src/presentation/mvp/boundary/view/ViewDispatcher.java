0c555c8d2084d116dfbe36e9871c84e120e349f4
francesco <frankgiak@gmail.com>
2015-08-12 12:40:10 +0200

    Aggiunto un pò di javadoc

--- Carloan/src/presentation/mvp/boundary/view/ViewDispatcher.java
+++ Carloan/src/presentation/mvp/boundary/view/ViewDispatcher.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import utility.ReaderXML;
 
 public class ViewDispatcher {
+	
 	private ReaderXML reader;
 	private String srcFXML;
 	private ReturnableStage stage;
@@ -11,7 +12,19 @@ public class ViewDispatcher {
 	public ViewDispatcher(String path){
 		reader= new ReaderXML(path);
 	}
-	
+	/**
+	 * <p>Recupera l'indirizzo in cui si trova la finestra da far mostrare e poi la passa al {@link ReturnableStage} per caricarla.</p>
+	 * @param serviceName
+	 * @param parameter
+	 * @return
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	public Object dispatch(String serviceName,Object parameter) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException{
 	    	srcFXML= reader.read_UI(serviceName);
 	    	stage= new ReturnableStage(srcFXML,parameter);
