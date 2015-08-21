c20648d017c8fdc349c421ea2a0731a746e642cb
Gelidcrow <gelidcrow@gmail.com>
2015-08-21 10:58:46 +0200

    --

--- Carloan/src/presentation/mvp/view/controller/generale/SchermataGenerale.java
+++ Carloan/src/presentation/mvp/view/controller/generale/SchermataGenerale.java
@@ -1,10 +1,9 @@
 package presentation.mvp.view.controller.generale;
 
-import integration.DAO.connection.Connection;
+
 
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -21,24 +20,18 @@ import presentation.mvp.view.controller.Schermata;
 import Errori.AlertView;
 import utility.Finestra;
 import utility.ParametriFXML;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Modality;
-import javafx.stage.WindowEvent;
 
 
 public class SchermataGenerale<T extends Entity> extends Schermata{
