ffd57b4f1b18677a8d7b6372f08072e2489d9cc6
Gelidcrow <gelidcrow@gmail.com>
2015-09-08 23:53:53 +0200

    e

--- Carloan/src/presentation/controller/CarLoanFC.java
+++ Carloan/src/presentation/controller/CarLoanFC.java
@@ -5,10 +5,10 @@ import java.lang.reflect.InvocationTargetException;
 import business.model.Exception.CommonException;
 
 public class CarLoanFC implements FrontController {
-	private CarLoanAC carloanAC;
+	private ApplicationController carloanAC;
 	@Override
 	public Object processRequest(String request, Object parameter) throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException,CommonException {   
-		carloanAC = (CarLoanAC) ApplicationControllerFactory.getApplicationController();
+		carloanAC = ApplicationControllerFactory.getApplicationController();
 		return carloanAC.handleRequest(request, parameter);
 	}
 }
